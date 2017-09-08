5b28640008b73986c2c6af8feae700aa89343aca
Jinghao Yu <markyudan5@gmail.com>
2017-09-08 11:04:41 +0800

    1.12

---
+++ C1/exercise/exercise_1_12.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
+
+main(){
+    int c, state;
+    state = OUT;
+    
+    while ((c = getchar()) != EOF){
+        if (state == IN){
+            if (c == ' ' || c == '\n' || c == '\t'){
+                putchar('\n');
+                state = OUT; 
+            }
+            else{
+                putchar(c);
+            }
+        }
+        else if (state == OUT){
+            if (c != ' ' || c != '\n' || c != '\t'){
+                putchar(c);
+                state = IN;
+            }
+        }
+    }
+}
