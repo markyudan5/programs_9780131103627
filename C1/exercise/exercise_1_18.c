6b6f563d4483ac32bc3fb99836915c9f2ff7428c
Jinghao Yu <markyudan5@gmail.com>
2017-09-12 23:04:02 +0800

    1.18 70%

--- C1/exercise/exercise_1_18.c
+++ C1/exercise/exercise_1_18.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #define MAXLINE 1000
-int removetrailblanks(char store[], char input[], int input_len);
+int removetrailblanks(char store[], char input[], int store_len, int input_len);
 int main(){
     char lines[MAXLINE];
     char temp_line[MAXLINE];
@@ -12,7 +12,7 @@ int main(){
     while ((c = getchar()) != EOF && i < (MAXLINE - 1)){
         if (c == '\n' && i >= 80){
             temp_line[i] = '\0';
-            lines_len = append(lines, temp_line, lines_len, i);
+            lines_len = removetrailblanks(lines, temp_line, lines_len, );
             i = 0;
         }
         else if (c == '\n' && i < 80){
@@ -24,13 +24,23 @@ int main(){
         }
     } 
     lines[++lines_len] = '\0';
-
+    printf("%s\n", lines);
     return 0;
 }
 /* remove trail blanks and tabs of the input string and append it to store[]
  * if it is a blank line, append nothing
  * return the length of the store[]
  */
-int removetrailblanks(char store[], char input[], int input_len){
-
+int removetrailblanks(char store[], char input[], int store_len, int input_len){
+    int i = store_len;
+    int j = input_len - 1;
+    while (j > 0 && (input[j] == ' ' || input[j] == '\t')){
+        if (input[j - 1] != ' ' && input[j - 1] != '\t'){
+            input[j] = '\0';
+            break;
+        }
+        else{
+            --j;
+        }
+    }
 }
