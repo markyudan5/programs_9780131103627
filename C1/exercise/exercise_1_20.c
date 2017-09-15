40c46c73b85821bbf95f7c01e2da0b601530f90a
Jinghao Yu <markyudan5@gmail.com>
2017-09-15 14:35:01 +0800

    save for next time

--- C1/exercise/exercise_1_20.c
+++ C1/exercise/exercise_1_20.c
@@ -17,17 +17,22 @@ void detab(){
     int index = 0;
 
     while ((c = getchar()) != EOF){
+        printf("index:%d\n", index);
         if (c == '\t'){
             int i;
             for (i = 0; i < SPACES; ++i){
-                new[index + i + 1] = ' ';
+                new[index + i] = ' ';
             }
             index = index + i + 1;
         }
         else{
             new[index] = c;
+            printf("%c, %d\n", new[index], index);
+            printf("in loop%s\n", new);
             ++index;
         }
     } 
+    new[index] = '\0';
+    printf("final index:%d\n", index);
     printf("%s\n", new);
 }
